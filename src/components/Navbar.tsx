e1a22b9ba6300642290718be3efbc0e30d82733c
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-23 22:45:07 +0300

    route handling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,8 @@ const Navbar = () => {
         <h5 className='p-2   text-xl font-bold  '>Notes App</h5>
     </div>
     <div className='flex justify-around items-center col-span-3 gap-3 '>
-        <ButtonLink href={'/auth'} name={'Register'} className={' p-2 text-center rounded-xl w-1/3 '} />
-        <ButtonLink href={'/login'} name={'Login'} className={' p-2 text-center rounded-xl w-1/3 '} />
+        <ButtonLink href={'/auth/register'} name={'Register'} className={' p-2 text-center rounded-xl w-1/3 '} />
+        <ButtonLink href={'/auth/login'} name={'Login'} className={' p-2 text-center rounded-xl w-1/3 '} />
     </div>
 
     </GridContainer>
