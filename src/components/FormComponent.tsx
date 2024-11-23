ea5aa842d00083bf648c31d96dfdff9ee2584189
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-23 22:02:34 +0300

    Dynamic form input creation

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const FormInput = () => {
-    return (
-        <div>
-        
-        </div>
-    )
+const FormInput = ({name,value,type,placeholder,onChange,onBlur,FormikError}:{FormikError:string,name:string,value:string,type:string,placeholder:string,onChange:void,onBlur:void}) => {
+    return <>
+        <label htmlFor={name}></label>
+        <input className='' type={type}  placeholder={placeholder} id={name} name={name} value={value} onChange={onChange} onBlur={onBlur}/>
+        <h3>{FormikError}</h3>
+    </>
 }
 
-export default FormComponent
+export default FormInput
