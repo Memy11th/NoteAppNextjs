5e4a4905cb2ccdccdaf635dea038b9375c210700
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-24 22:33:03 +0300

    Get all notes fn

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -30,5 +30,16 @@ export const handleLogin = async(formikValues:LoginForm)=>{
     return data
 };
 
+export const getallNotes = async(token:string)=>{
+  const res = await fetch('https://note-sigma-black.vercel.app/api/v1/notes/allNotes',{
+    method:'GET',
+    headers:{
+      'Content-Type': 'application/json',
+      token: `3b8ny__${token}`
+    }
+  });
+    const data = await res.json();
+    return data
+}
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+'use client'
+import { getallNotes } from "@/Api/Api"
+import { useEffect } from "react"
 
 export default function Home() {
+
+    const getNotes = async ()=>{
+        const token = localStorage.getItem('token') || '';
+        const res = await getallNotes(token);
+        console.log(res)
+    }
+
+    useEffect( () => {
+        getNotes();
+    }, [] )
     return <>
         <div className="min-h-screen">
 
