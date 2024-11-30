3139c70d7e1244e244d020fa865ee7cd7cafe282
Memy11th <abdelrahmanabdue11@gmail.com>
2024-11-30 19:23:08 +0300

    Action button functionality done

--- src/components/AddNote.tsx
+++ src/components/AddNote.tsx
@@ -1,11 +1,12 @@
 'use client'
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
+import { useFormik } from 'formik';
+import FormInput from './FormComponent';
 
 const style = {
   position: 'absolute',
@@ -15,15 +16,41 @@ const style = {
   width: 400,
   bgcolor: 'background.paper',
   border: '2px solid #000',
+  borderRadius: 5,
   boxShadow: 24,
   p: 4,
 };
+
+const label= [
+  {
+    name:'title',
+    placeholder:'Title',
+    type:'text'
+  },
+  {
+    name:'content',
+    placeholder:'Content',
+    type:'text'
+  }
+]
 
 export default function ActionButton() {
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handleSubmit = (formikValues)=>{
+      
+  }
+
+  const formik = useFormik({
+    initialValues:{
+      title:'',
+      content:''
+    },
+    onSubmit: handleSubmit
+  })
 
 
   return <React.Fragment>
@@ -40,12 +67,20 @@ export default function ActionButton() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+            <form className='flex flex-col gap-4' onSubmit={formik.handleSubmit} >
+              {label.map((item , index)=>(<FormInput key={index} {...item}
+              value={formik.values[item.name as keyof typeof formik.values]}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              FormikError={formik.errors[item.name as keyof typeof formik.errors] as string | undefined}
+              touched={formik.touched[item.name as keyof typeof formik.touched]}
+              className='outline-none border p-2 rounded-md'
+              />))}  
+
+              <button type='submit' className='p-2 bg-green-700 text-white rounded-md'>
+                Add note
+              </button>                
+            </form>
         </Box>
       </Modal>
     </div>
--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const FormInput = ({name,value,type,placeholder,onChange,onBlur,FormikError,touched}
-    :{FormikError?:string|undefined,touched?:boolean,name:string,value:string,type:string,placeholder:string,onChange:(event: React.ChangeEvent<HTMLInputElement>) => void,onBlur:(event: React.FocusEvent<HTMLInputElement>) => void}) => {
+const FormInput = ({name,value,type,placeholder,onChange,onBlur,FormikError,touched,className}
+    :{FormikError?:string|undefined,touched?:boolean,name:string,className?:string,value:string,type:string,placeholder:string,onChange:(event: React.ChangeEvent<HTMLInputElement>) => void,onBlur:(event: React.FocusEvent<HTMLInputElement>) => void}) => {
     return <>
         <label htmlFor={name}>{name}</label>
-        <input className='' type={type}  placeholder={placeholder}  name={name} value={value || ''} onChange={onChange} onBlur={onBlur}/>
+        <input className={className} type={type}  placeholder={placeholder}  name={name} value={value || ''} onChange={onChange} onBlur={onBlur}/>
         {FormikError && touched && <h3>{FormikError}</h3>}
     </>
 }
